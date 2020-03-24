c57e324fdcddad66ebe1c0d3e5ba2aa1176d3979
Iago Virgílio <iagovirgilio@gmail.com>
2020-03-24 11:42:55 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import params from './src/params';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o mines!</Text>
+        <Text style={styles.welcome}>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
