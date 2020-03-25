d875f66567278a843757e07894e70786d33c09f1
Iago Virgílio <iagovirgilio@gmail.com>
2020-03-25 10:15:47 -0300

    Criado campos e mina

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,19 @@ export default class App extends Component {
         <Text style={styles.welcome}>
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={4} />
+        <Field opened nearMines={5} />
+        <Field opened nearMines={6} />
+        <Field opened nearMines={7} />
+        <Field opened nearMines={8} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
       </View>
     );
   }
